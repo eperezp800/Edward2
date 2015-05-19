adb2503bd47777182ca943ef90b7d0dd3b45c8e4
eperezp800 <edw10@hotmail.es>
2015-05-18 20:23:13 -0600

    envio2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hola Mundo");
+            System.Console.WriteLine("Hola Gente");
             System.Console.ReadKey();
         }
     }
